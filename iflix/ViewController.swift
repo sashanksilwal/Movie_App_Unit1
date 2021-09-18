58f749d084d643934d7f310033f2c76c83455388
sashanksilwal <sashanksilwal@hotmail.com>
2021-09-18 13:04:52 -0400

    Initial Commit

---
+++ iflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iflix
+//
+//  Created by Sashank Silwal on 9/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
